55f74a8159a6643117b812128ab52d622f3ef4b4
no <no>
2024-03-10 15:23:14 -0700

    setup.sh works

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
-# Setup environment
+#!/bin/bash
+
+# Constants
 GREEN='\033[0;32m'
-echo -e "${GREEN}Creating virtual environment"
+NC='\033[0m'
+
+# Setup environment
+echo -e "${GREEN}Creating virtual environment${NC}"
 python3 -m venv venv
 source venv/bin/activate
-echo -e "${GREEN}Installing packages"
+
+echo -e "${GREEN}Installing packages${NC}"
 pip install --upgrade pip
 pip install -r requirements.txt
-echo -e "${GREEN}Finished"
+
+echo -e "${GREEN}Installing pre-commit hooks${NC}"
+pre-commit install
+
+echo -e "${GREEN}Finished${NC}"
