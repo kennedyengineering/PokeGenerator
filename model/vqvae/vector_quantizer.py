14a56cae4a131ca45520c808fb47ebf91c0141fe
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-03-18 16:41:49 -0700

    Refactor imports in vector_quantizer.py

--- model/vqvae/vector_quantizer.py
+++ model/vqvae/vector_quantizer.py
@@ -1,11 +1,11 @@
 # vqvae/vector_quantizer.py
 
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-from tensorflow import keras
+# from tensorflow import keras
 from tensorflow.keras import layers
-import tensorflow_probability as tfp
+# import tensorflow_probability as tfp
 import tensorflow as tf
 
 
