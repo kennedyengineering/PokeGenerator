dbe0460305df9fd9e7c995ca474ba4ec93915b81
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-03-20 10:51:58 -0700

    Refactor latent_diffusion_model architecture in latent_test.py to improve performance

--- model/latent_test.py
+++ model/latent_test.py
@@ -8,6 +8,7 @@ import glob
 import variational_autoencoder as vae
 from latent_diffusion import build_reverse_process_mlp_model, train_model, sample
 from datetime import datetime
+from pathlib import Path
 
 def display_images(images, titles=None, cols=5, figsize=(20, 10)):
     """
@@ -78,23 +79,45 @@ def generate_images(decoder, num_images=10, latent_dim=2048):
     # Display the generated images
     display_images(generated_images, cols=num_images, figsize=(20, 10))
 
+def display_image_pairs(originals, reconstructions, page=1, pairs_per_page=4):
+    """Displays original and reconstructed image pairs in a gallery format."""
+    total_pairs = len(originals)
+    start = (page - 1) * pairs_per_page * 2
+    end = start + pairs_per_page * 2
+    images_to_show = np.concatenate((originals[start//2:end//2], reconstructions[start//2:end//2]))
+    titles = ['Original' if i < pairs_per_page else 'Reconstructed' for i in range(pairs_per_page * 2)]
+
+    cols = 4  # Two columns per pair, original and reconstructed
+    rows = (len(images_to_show) + cols - 1) // cols
+    plt.figure(figsize=(10, 2.5 * rows))
+    for i, image in enumerate(images_to_show):
+        plt.subplot(rows, cols, i + 1)
+        plt.imshow(image, cmap='gray')  # Adjust as needed
+        plt.title(titles[i % pairs_per_page])
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
 def main():
     with open("training_config.json", "r") as f:
         config = json.load(f)
     images = load_dataset(config)
 
-    model_files = glob.glob("checkpoints/*.keras")
-    latest_model_file = max(model_files, key=os.path.getmtime)
-    print(f"Attempting to load model from: {latest_model_file}")
-    variational_autoencoder = tf.keras.models.load_model(latest_model_file)
+    ae_model_files = glob.glob("checkpoints/model_*.keras")
+    latest_ae_model_file = max(ae_model_files, key=os.path.getmtime)
+    print(f"Attempting to load model from: {latest_ae_model_file}")
+    variational_autoencoder = tf.keras.models.load_model(latest_ae_model_file)
+    variational_autoencoder.summary()
 
     encoder = variational_autoencoder.get_layer('encoder')
     decoder = variational_autoencoder.get_layer('decoder')
 
-    # encoder.summary()
-    # decoder.summary()
+    # Show Reconstruction
+    reconstructions = variational_autoencoder.predict(images)
+
+    # Show in gallery format of original compare to reconstructed
+    display_image_pairs(images, reconstructions, page=1)
     
-    # encoded_imgs = encoder.predict(images[:2]) 
     latent_vectors = encoder.predict(images)
     z_mean, z_log_var = latent_vectors
 
@@ -109,39 +132,53 @@ def main():
     latent_diffusion_model = build_reverse_process_mlp_model(input_dim=sampled_latent_vectors.shape[1], num_layers=3, num_hidden=512, T=1000)
     latent_diffusion_model.summary()
 
-    # Training
+    # # Training
     T = 1000
     betas = np.linspace(1e-4, .02, T)
     sigmas = np.sqrt(betas)
     alphas = 1 - betas
     alphas_cumprod = np.cumprod(alphas, axis=-1)
 
-    batch_size = 32
-
-    model = train_model(sampled_latent_vectors, batch_size, T, alphas_cumprod, latent_diffusion_model, epochs=config['epochs'])
+    # Train or Load Model
+    if config["train_latent_model"]:
+        ld_model = train_model(
+            sampled_latent_vectors, 
+            batch_size=config["batch_size"], 
+            T=T, 
+            alphas_cumprod=alphas_cumprod, 
+            model=latent_diffusion_model, 
+            epochs=config['epochs']
+        )
+    else:
+        latent_model_files = glob.glob("checkpoints/latent_model_*.keras")
+        latest_latent_model_file = max(latent_model_files, key=os.path.getmtime)
+        print(f"Attempting to load model from: {latest_latent_model_file}")
+        ld_model = tf.keras.models.load_model("model/checkpoints/latent_model_*.keras")
+        ld_model.summary()
 
     # Sample
-    ld_sampled_vectors = sample(model, sampled_latent_vectors.shape, T, sigmas, alphas, alphas_cumprod) 
+    ld_sampled_vectors = sample(
+        ld_model, 
+        sampled_latent_vectors.shape, 
+        T, 
+        sigmas, 
+        alphas, 
+        alphas_cumprod
+    ) 
     decode_images = decoder.predict(ld_sampled_vectors)
 
+    print("Decoded Images Shape:", decode_images[0].shape)
     
     # # Display the generated images
     display_images(decode_images, cols=10, figsize=(20, 10))
 
-    checkpoint_directory = config["checkpoint_directory"]
+    checkpoint_directory = Path(config["checkpoint_directory"])
+    checkpoint_directory.mkdir(exist_ok=True)
     # Save the model
-    model.save(
+    ld_model.save(
         checkpoint_directory
         / ("latent_model_" + datetime.now().strftime("%Y-%m-%d-%H:%M:%S%z") + ".keras")
     )
-
-    # plot_interpolated_images(decoder, z_mean)
-
-    # reconstructed_images = variational_autoencoder.predict(images[:10])
-    # display_images(reconstructed_images, titles=['Reconstructed'] * 10)
-
-    # # Generate and display random images from the latent space
-    # generate_images(decoder, num_images=10, latent_dim=512)
 
 if __name__ == "__main__":
     main()
